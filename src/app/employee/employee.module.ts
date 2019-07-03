a9572c45a71b1db82f546cc74331cd12e41c274d
Njegos Rajcevic <njegosr406@gmail.com>
2019-07-03 18:01:51 +0200

    employee module created

---
+++ src/app/employee/employee.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmployeeRoutingModule } from './employee-routing.module';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+
+
+@NgModule({
+  declarations: [
+    EmployeeListComponent
+  ],
+  imports: [
+    CommonModule,
+    EmployeeRoutingModule
+  ],
+  exports: [
+    EmployeeListComponent
+  ]
+})
+export class EmployeeModule { }
